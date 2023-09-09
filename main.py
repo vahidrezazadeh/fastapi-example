6742a189c5a3d0e5b1fd167d5db3e36c92a9a169
Vahid Rezazadeh <vrh.fardap@gmail.com>
2023-09-09 14:04:41 +0330

    main example file

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes.api import router
+
+from settings import settings
+
+def initApplication() -> FastAPI:
+
+    ## Create FastApi App
+    fastApiApp = FastAPI()
+
+    fullApiPrefix = str("/%s/%s" %(settings.apiPrefix , settings.apiVersion))
+    ## Mapping api routes
+    fastApiApp.include_router(router,prefix=fullApiPrefix)
+
+    ## Allow cors
+    fastApiApp.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.ALLOWED_HOSTS or ["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return fastApiApp
+
+
+app = initApplication()
