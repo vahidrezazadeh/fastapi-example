de858c44db71a35d1937afb43e49188f94eb2af0
Vahid Rezazadeh <vrh.fardap@gmail.com>
2023-09-11 10:15:03 +0330

    jst Service and controllers usage

--- app/api/controllers/auth/login.py
+++ app/api/controllers/auth/login.py
@@ -2,13 +2,17 @@ from fastapi import APIRouter, Depends, HTTPException
 
 from app.schemas.auth.LoginSchema import LoginSchema
 
+from services.AuthService import AuthService
+
 router = APIRouter(
     tags=["Login Api"],
     prefix='/login'
     )
 
 
-@router.post("/", response_model=str)
-def loginUser(body : LoginSchema):
+@router.post("/", response_model=dict)
+async def loginUser(body : LoginSchema):
     print(body)
-    return "Login User API Response"
+    userId = 1
+    jwtData =await AuthService.generateTokens(userId)
+    return jwtData
---
+++ services/AuthService.py
@@ -0,0 +1,21 @@
+from utils.jwt import create_access_token , create_refresh_token
+
+
+class AuthService:
+    @staticmethod
+    async def generateTokens(user_id : int) -> dict: 
+
+        access_token, access_token_expire_time = await create_access_token(
+                    str(user_id), multi_login=False
+                )
+        
+        refresh_token, refresh_token_expire_time = await create_refresh_token(
+                    str(user_id), multi_login=False
+                )
+        
+        return {
+            "access_token": access_token,
+            "access_token_expire" : access_token_expire_time,
+            "refresh_token": refresh_token,
+            "refresh_token_expire" : refresh_token_expire_time,
+        }
